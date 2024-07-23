bd226913c78f99f1174a73ebc40139258067a2ec
Zakrieva <zakrieva34t@mail.ru>
2024-07-23 19:56:47 +0300

    fix:Product

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -2,19 +2,20 @@ import Product from "./product/Product"
 
 const Products = () => {
     const products = [
-        { model: 'Samsung ...', img: './product/', price: '$1000', href: '#' },
-        { model: 'Samsung ...', img: './product/', price: '$1000', href: '#' },
-        { model: 'Samsung ...', img: './product/', price: '$1000', href: '#' },
-        { model: 'Samsung ...', img: './product/', price: '$1000', href: '#' },
-        { model: 'Samsung ...', img: './product/', price: '$1000', href: '#' },
-        { model: 'Samsung ...', img: './product/', price: '$1000', href: '#' }
+        { model: 'Samsung ...', img: './product/', price: '$1000', href: '#', id: 0 },
+        { model: 'Samsung ...', img: './product/', price: '$1000', href: '#', id: 1 },
+        { model: 'Samsung ...', img: './product/', price: '$1000', href: '#', id: 3 },
+        { model: 'Samsung ...', img: './product/', price: '$1000', href: '#', id: 4 },
+        { model: 'Samsung ...', img: './product/', price: '$1000', href: '#', id: 5 },
+        { model: 'Samsung ...', img: './product/', price: '$1000', href: '#', id: 6 }
     ]
-    return (<div className="product-items">
+    return (
+        <div className="product-items">
 
-        {products.map(({ model, img, price, href }) => {
-            <Product model={model} img={img} price={price} href={href} />
-        })}
-    </div>
+            {products.map(({ model, img, price, href }) =>
+                <Product model={model} img={img} price={price} href={href} key={id} />
+            )}
+        </div>
     )
 }
 export default Products;
