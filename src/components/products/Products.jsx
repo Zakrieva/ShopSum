8abe5dea0f3207585915912784c3bcf9d2d5cc97
Zakrieva <zakrieva34@mail.ru>
2024-07-25 08:46:23 +0300

    add products

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,19 +1,24 @@
 import Product from "./product/Product"
-
 const Products = () => {
     const products = [
-        { model: 'Samsung ...', img: './product/', price: '$1000', href: '#', id: 0 },
-        { model: 'Samsung ...', img: './product/', price: '$1000', href: '#', id: 1 },
-        { model: 'Samsung ...', img: './product/', price: '$1000', href: '#', id: 3 },
-        { model: 'Samsung ...', img: './product/', price: '$1000', href: '#', id: 4 },
-        { model: 'Samsung ...', img: './product/', price: '$1000', href: '#', id: 5 },
-        { model: 'Samsung ...', img: './product/', price: '$1000', href: '#', id: 6 }
+        { model: 'Samsung Galaxy S24 Ultra', img: './product/s24ultra.jpg', price: '$1000', href: '#', },
+        { model: 'Samsung Galaxy S24 Ultra', img: './product/s24ultra.jpg', price: '$1000', href: '#', },
+        { model: 'Samsung Galaxy S24 Ultra', img: './product/s24ultra.jpg', price: '$1000', href: '#', },
+        { model: 'Samsung Galaxy S24 Ultra', img: './product/s24ultra.jpg', price: '$1000', href: '#', },
+        { model: 'Samsung Galaxy S24 Ultra', img: './product/s24ultra.jpg', price: '$1000', href: '#', },
+        { model: 'Samsung Galaxy S24 Ultra', img: './product/s24ultra.jpg', price: '$1000', href: '#', },
+        { model: 'Samsung Galaxy S24 Ultra', img: './product/s24ultra.jpg', price: '$1000', href: '#', },
+        { model: 'Samsung Galaxy S24 Ultra', img: './product/s24ultra.jpg', price: '$1000', href: '#', },
+        { model: 'Samsung Galaxy S24 Ultra', img: './product/s24ultra.jpg', price: '$1000', href: '#', },
+        { model: 'Samsung Galaxy S24 Ultra', img: './product/s24ultra.jpg', price: '$1000', href: '#', },
+        { model: 'Samsung Galaxy S24 Ultra', img: './product/s24ultra.jpg', price: '$1000', href: '#', },
+        { model: 'Samsung Galaxy S24 Ultra', img: './product/s24ultra.jpg', price: '$1000', href: '#', }
     ]
     return (
         <div className="product-items">
 
             {products.map(({ model, img, price, href }) =>
-                <Product model={model} img={img} price={price} href={href} key={id} />
+                <Product model={model} img={img} price={price} href={href} />
             )}
         </div>
     )
--- src/components/products/product/Product.jsx
+++ src/components/products/product/Product.jsx
@@ -1,15 +1,14 @@
 
 const Product = ({ model, img, price, href }) => {
-    console.log(123);
     return (
-        <div className="product-item">
+        <div className="product-item" >
             <div className="product-item-img">
                 <img src={img}></img>
             </div>
 
             <div className="product-list">
                 <a href={href}>{model}</a>
-                <span class="price">{price}</span>
+                <span className="price">{price}</span>
             </div>
         </div>)
 }
